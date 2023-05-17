45c0665282b234c577a0c483ad3a807da3265e14
Rehab Mohamed <Rehab.Mohamed@itworx.com>
2023-05-17 23:11:49 +0300

    task9

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-let data = [{
-    pic: "jacket.jpg",
-    name: "Mohan",
-    desc: "Recycle Boucle Knit Cardigan Pink",
-    price: "$120",
-    id: 1,
-    size: "S",
-    count: "0",
-},
-];
-
-const selectedItem = {
-    name: "Mohan",
-    desc: "Recycle Boucle Knit Cardigan Pink",
-    price: "$120",
-    id: 1,
-    size: "S",
-    count: "0",
-  };
 
 
+
+ 
+  
 
 function loadContent() {
 
@@ -36,56 +20,61 @@ function loadContent() {
         document.querySelector(".main-content").append(cartContainer);
 
     } else {
-
-        const detailsContainer = document.createElement("div");
-        detailsContainer.innerHTML = `
-        <div>
-        <img class="img-fluid" src="jacket.jpg"/>
-        <h3 class="pro-title mt-4">${data[0].name}</h3>
-        <p class="pro-description">${data[0].desc}</p>
-        <div class="pro-price">${data[0].price} </div>
-        <div class="container-fluid colorAndSize-wrapper p-0 my-3">
-        <div class="row">
-                <div class="col-6">
-                    <div class="pro-colors d-flex justify-content-between">
-                        <h6>Color</h6>
-                        <div class="black b-selector" onclick="blackSelector(this)"></div>
-                        <div class="orange o-selector" onclick="orangeSelector(this)"></div>
-                        <div class="grey g-selector" onclick="greySelector(this)"></div>
+       
+        $.getJSON("https://raw.githubusercontent.com/Rehabmohamed19/ITW-Task9/master/data.json", function(result){
+            selectedItem=result[0];
+            localStorage.setItem("selectedItem", JSON.stringify(selectedItem));
+            const detailsContainer = document.createElement("div");
+            detailsContainer.innerHTML = `
+            <div>
+            <img class="img-fluid" src="${selectedItem.pic}"/>
+            <h3 class="pro-title mt-4">${selectedItem.name}</h3>
+            <p class="pro-description">${selectedItem.desc}</p>
+            <div class="pro-price">${selectedItem.price} </div>
+            <div class="container-fluid colorAndSize-wrapper p-0 my-3">
+            <div class="row">
+                    <div class="col-6">
+                        <div class="pro-colors d-flex justify-content-between">
+                            <h6>Color</h6>
+                            <div class="black b-selector" onclick="blackSelector(this)"></div>
+                            <div class="orange o-selector" onclick="orangeSelector(this)"></div>
+                            <div class="grey g-selector" onclick="greySelector(this)"></div>
+                        </div>
                     </div>
-                </div>
-                
-                <div class="col-6">
-                    <div class="pro-sizes d-flex justify-content-between">
-                        <h6>Size</h6>
-                        <div class="small text-center sm-selector" onclick="smallSelector(this)">S</div>
-                        <div class="medium text-center me-selector" onclick="mediumSelector(this)">M</div>
-                        <div class="large text-center la-selector" onclick="largeSelector(this)">L</div>
+                    
+                    <div class="col-6">
+                        <div class="pro-sizes d-flex justify-content-between">
+                            <h6>Size</h6>
+                            <div class="small text-center sm-selector" onclick="smallSelector(this)">S</div>
+                            <div class="medium text-center me-selector" onclick="mediumSelector(this)">M</div>
+                            <div class="large text-center la-selector" onclick="largeSelector(this)">L</div>
+                        </div>
                     </div>
-                </div>
-        </div>
-        <div class="shown-items">
-        <div class="row d-flex mt-4" style="background-color: black;">
-
-                <i class="fa-solid fa-plus col-2 plus" style="color: white;" onclick="addToBasket(this)"></i>
-                <button type = "button" onclick="addToBasket(this)" class="addToBasket col-8 d-flex justify-content-start"> ADD TO BASKET </button>
-                <i class="fa-regular fa-heart col-2 heart" style="color: #ffffff;"></i>
-        </div>
-        </div>
-        <div class="hidden-items">
-        <div class="row d-flex justify-content-center" style="background-color: black;">
+            </div>
+            <div class="shown-items">
+            <div class="row d-flex mt-4" style="background-color: black;">
+    
+                    <i class="fa-solid fa-plus col-2 plus" style="color: white;" onclick="addToBasket(this)"></i>
+                    <button type = "button" onclick="addToBasket(this)" class="addToBasket col-8 d-flex justify-content-start"> ADD TO BASKET </button>
+                    <i class="fa-regular fa-heart col-2 heart" style="color: #ffffff;"></i>
+            </div>
+            </div>
+            <div class="hidden-items">
+            <div class="row d-flex justify-content-center" style="background-color: black;">
+                    
+                        <i class="fa-solid fa-minus col-5 d-flex justify-content-end counter-minus" onclick="removeFromBasket(this)" style="color: white; "></i>
+                        <div class="counter col-2 d-flex justify-content-center" id="counter">0</div>
+                        <i class="fa-solid fa-plus col-5 d-flex justify-content-start counter-plus" onclick="addToBasket(this)" style="color: white; "></i>
+                    
+            </div>
+            </div>
+            </div>    
+            </div>
                 
-                    <i class="fa-solid fa-minus col-5 d-flex justify-content-end counter-minus" onclick="removeFromBasket(this)" style="color: white; "></i>
-                    <div class="counter col-2 d-flex justify-content-center" id="counter">0</div>
-                    <i class="fa-solid fa-plus col-5 d-flex justify-content-start counter-plus" onclick="addToBasket(this)" style="color: white; "></i>
-                
-        </div>
-        </div>
-        </div>    
-        </div>
-            
-        `;
-        document.querySelector(".main-content").append(detailsContainer);
+            `;
+            document.querySelector(".main-content").append(detailsContainer);
+        });
+      
     }
 };
 
